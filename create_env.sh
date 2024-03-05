d89b8be9858b334f07ee0e41096784a339cc10a4
Kaspar Beelen <kasparbeelen@mac-58294.local>
2024-03-05 13:02:32 +0000

    add create_env.sh

---
+++ create_env.sh
@@ -0,0 +1,7 @@
+cd /content/heritageweaver/
+pip install -qr requirements.txt
+# download data database and some additional metadata
+gdown 1ZKZhmjVDO2U2cYRmCvu_phGgQAFY7__R
+gdown 1BkIfIxksVjXHeWYj2JM-dYR6Ibprz8SN
+# unzip archive with vector database
+!unzip -qq ce_comms_db.zip
