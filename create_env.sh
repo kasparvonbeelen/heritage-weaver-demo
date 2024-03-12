f91ed2e4dde7fa841044a5e99142295e92915e68
Kaspar Beelen <kasparbeelen@mac-58294.local>
2024-03-12 16:32:05 +0000

    upload communications csv

--- create_env.sh
+++ create_env.sh
@@ -3,6 +3,5 @@ pip install -qr requirements.txt
 # download data database and some additional metadata
 cd /content/
 gdown 1ZKZhmjVDO2U2cYRmCvu_phGgQAFY7__R
-gdown 1BkIfIxksVjXHeWYj2JM-dYR6Ibprz8SN
 # unzip archive with vector database
 unzip -qq ce_comms_db.zip
