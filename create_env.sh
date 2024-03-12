3ab0d6e9b0b536743a9cf70fb0ca520c0953ce17
Kaspar Beelen <kasparbeelen@mac-58294.local>
2024-03-12 17:21:33 +0000

    renew share code

--- create_env.sh
+++ create_env.sh
@@ -1,8 +1,8 @@
 cd /content/heritageweaver/
 pip install -qr requirements.txt
-!pip install --upgrade --no-cache-dir gdown
 # download data database and some additional metadata
 cd /content/
+pip install --upgrade --no-cache-dir gdown
 gdown 1egflWfa4R_ZHCTAyKlbElGeF8U66Z4Rr
 # unzip archive with vector database
 unzip -qq ce_comms_db_copy.zip
