c4f4fc516657bd41d069237e9801a12587a2710d
Kaspar Beelen <kasparbeelen@mac-58294.local>
2024-04-11 15:53:45 +0100

    fix unzip bug

--- create_env.sh
+++ create_env.sh
@@ -5,4 +5,4 @@ cd /content/
 pip install --upgrade --no-cache-dir gdown
 gdown 19a91qQXxeU0zZqv49UrP7h2BZ82lEiUD
 # unzip archive with vector database
-unzip -qq ce_comms_db_copy.zip
+unzip -qq ce_comms_db_08042024.zip
