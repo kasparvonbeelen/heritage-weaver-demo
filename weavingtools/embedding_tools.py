e6d29f3095f5e35f305dbfaebf9122c0bb370b09
kasparvonbeelen <kasparbeelen@mac-58294.local>
2024-08-02 17:13:58 +0100

    update codebase data ingestion

--- weavingtools/embedding_tools.py
+++ weavingtools/embedding_tools.py
@@ -1,15 +1,22 @@
 from transformers import AutoProcessor, AutoModel
+import chromadb
+from typing import NewType
+from concurrent.futures import ThreadPoolExecutor
+import multiprocessing
+import importlib
+from chromadb.utils.data_loaders import ImageLoader
 from chromadb import Documents, EmbeddingFunction, Embeddings
 from PIL import JpegImagePlugin
 import torch
 import numpy as np
+import pandas as pd
 from spacy.lang.en import English 
 from tqdm.notebook import tqdm
-from keybert import KeyBERT
+from typing import Dict,  List, Tuple, Union, Literal
 
 nlp = English()
 nlp.add_pipe('sentencizer')
-kw_model = KeyBERT()
+#kw_model = KeyBERT()
 
 class SigLIPEmbedder(EmbeddingFunction):
     """embedding function for indexing (batches of) text or images"""
@@ -45,46 +52,141 @@ class SigLIPEmbedder(EmbeddingFunction):
         embeddings = embeddings / embeddings.norm(p=2, dim=-1, keepdim=True)
         
         return embeddings.tolist()
-    
 
-def batchify(content_list, n=20):
-    for i in range(0, len(content_list), n):
-        yield np.array(content_list[i:min(i+n,len(content_list))])
+class ImageLoaderRGB():
+    def __init__(self, max_workers: int = multiprocessing.cpu_count()) -> None:
+        try:
+            self._PILImage = importlib.import_module("PIL.Image")
+            self._max_workers = max_workers
+        except ImportError:
+            raise ValueError(
+                "The PIL python package is not installed. Please install it with `pip install pillow`"
+            )
 
-def reshape_text_batch(batch, collection):
-    
-    content = [[b[0],b[1],s,b[3]] 
-                       for b in list(batch) 
-                           for s in nlp(str(b[1])+ '. ' + str(b[2])).sents if len(s) > 2 # str(b[1])+ ', '+
-                                  ] # parameter here
-    
-    metadatas = [{'record_id':e[0],
-                 'name':e[1],
-                 #'img_path': str(e[4]),
-                 'img_url': str(e[3]),
-                 'input_modality': 'text',
-                 'sentence': str(e[2]),
-                 'collection': collection
-                        }
-                    for e in content
-                    ]
-    content = [str(c[2]) for c in content]
-    return content,metadatas
+    def _load_image(self, uri):
+        #return np.array(self._PILImage.open(uri)) if uri is not None else None
+        return np.array(self._PILImage.open(uri).convert('RGB')) if uri is not None else None
+
+    def __call__(self, uris):
+        with ThreadPoolExecutor(max_workers=self._max_workers) as executor:
+            return list(executor.map(self._load_image, uris))
 
 
+ChromaDB = NewType('ChromaDB', chromadb.PersistentClient)
 
-def reshape_image_batch(batch, collection):
+def load_db(name: str="ce_comms_db", checkpoint: str='google/siglip-base-patch16-224') -> ChromaDB:
+    siglip_embedder = SigLIPEmbedder(checkpoint)
+    client = chromadb.PersistentClient(path=name)
+    data_loader = ImageLoader()
+    collection_db = client.get_or_create_collection(name=name, 
+                                             metadata={"hnsw:space": "cosine"},
+                                             embedding_function=siglip_embedder, 
+                                             data_loader=data_loader
+                                            )
+    return collection_db
     
-    content = [str(i) for i in list(batch[:,-1])]
+
+def batchify(df: pd.DataFrame, batch_size: int=32) -> pd.DataFrame:
+    """split dataframe into smaller batches of size batch_size
+    Arguments:
+        df (pd.DataFrame): dataframe to be split
+        batch_size (int): size of each batch
+    Returns:
+        a generator of dataframes
+    """
+    for i in range(0, len(df), batch_size):
+        yield df.iloc[i:min(i+batch_size,len(df))]
+
+
+def text_batch(batch: pd.DataFrame) -> Tuple[list, list]:
+    """split the batch into content and metadatas for text indexing"""
+    batch['modality'] = 'text'
+    batch['sentence'] = batch.apply(lambda x: [x['name']]+[sent.text for sent in nlp(x.description).sents], axis=1)
+    batch_exploded = batch.explode('sentence')
+    content = [str(s) for s in batch_exploded.sentence]
+    metadatas = batch_exploded[['record_id','name','sentence','img_url','img_path','modality','collection']].to_dict(orient='records')
+    return content, metadatas    
+
+
+def image_batch(batch: pd.DataFrame) -> Tuple[list, list]:
+    """split the batch into content and metadatas for image indexing"""
+    content = [str(path) for path in batch.img_path.values.tolist()]
+    batch['modality'] = 'image'
+    metadatas = batch[['record_id','name','img_url','img_path','modality','collection']].to_dict(orient='records') # 'description',
+    return content, metadatas
+
+def get_ids(collection_db, content: list) -> list:
+    """get ids for the current batch of content to be indexed in the database
+    """
+    counter = collection_db.count()
+    ids = [str(i) for i in range(counter,counter+len(content))]
+    return ids
+
+def index_data(collection_db: ChromaDB, collection_df: pd.DataFrame, batch_size: int=32) -> None:
+    """index batch-wise a collection of text and images provided in a dataframe
+    Arguments:
+        collection_db (ChromaDB): the chromabd client database to be indexed
+        collection_df (pd.DataFrame): the dataframe containing the collection to be indexed
+        batch_size (int): the size of each batch
+    """
+    batches = batchify(collection_df, batch_size=batch_size)
+
+    for batch in tqdm(batches, total=collection_df.shape[0]//batch_size):
+        image_content, image_metadatas = image_batch(batch)
+        text_content, text_metadatas = text_batch(batch)
+        
+        ids = get_ids(collection_db, image_content)
+
+        collection_db.add(
+            ids = ids,
+            uris = image_content,
+            metadatas = image_metadatas
+        )
+
+        ids = get_ids(collection_db, text_content)
+        
+        collection_db.add(
+            ids = ids,
+            documents = text_content,
+            metadatas = text_metadatas
+        )
+
+# -------------------------------
+# ----- Redundant functions -----
+# -------------------------------
+
+# def reshape_text_batch(batch, collection):
     
-    metadatas = [{'record_id':e[0],
-                  'name':e[1],
-                  'img_path': str(e[4]),
-                  'img_url': str(e[3]),
-                  'input_modality':'image',
-                  'collection': collection
-                        }
-                    for e in batch
-                    ]
+#     content = [[b[0],b[1],s,b[3]] 
+#                        for b in list(batch) 
+#                            for s in nlp(str(b[1])+ '. ' + str(b[2])).sents if len(s) > 2 # str(b[1])+ ', '+
+#                                   ] # parameter here
     
-    return content,metadatas
+#     metadatas = [{'record_id':e[0],
+#                  'name':e[1],
+#                  #'img_path': str(e[4]),
+#                  'img_url': str(e[3]),
+#                  'input_modality': 'text',
+#                  'sentence': str(e[2]),
+#                  'collection': collection
+#                         }
+#                     for e in content
+#                     ]
+#     content = [str(c[2]) for c in content]
+#     return content,metadatas
+
+# def reshape_image_batch(batch, collection):
+    
+#     content = [str(i) for i in list(batch[:,-1])]
+    
+#     metadatas = [{'record_id':e[0],
+#                   'name':e[1],
+#                   'img_path': str(e[4]),
+#                   'img_url': str(e[3]),
+#                   'input_modality':'image',
+#                   'collection': collection
+#                         }
+#                     for e in batch
+#                     ]
+    
+#     return content,metadatas
---
+++ weavingtools/linkage_tools.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import numpy as np
+import scipy.spatial as sp
+from .embedding_tools import ChromaDB
+from matplotlib import pyplot as plt
+from typing import Dict,  List, Tuple, Union, Literal
+
+def retrieve_records(db: ChromaDB,coll: str,modality: str) -> ChromaDB:   
+    filters = {
+        "$and": [
+            {
+                "input_modality": {
+                    "$eq": modality
+                }
+            },
+            {
+                "collection": {
+                    "$eq" : coll
+                }
+            }
+            ]
+        }
+
+    return db.get(
+                where=filters,
+                include=['embeddings','metadatas'] 
+                    )
+
+
+def get_data(db: ChromaDB,
+             coll1: str, coll2: str, 
+             modality1: str, 
+             modality2: str) -> Dict[str, Union[str, List[str], np.ndarray]]: 
+    data1 = retrieve_records(db,coll1, modality1)
+    data2 = retrieve_records(db,coll2, modality2)
+    
+    inputs = dict()
+    
+    for i,(name,data) in enumerate([(coll1, data1), (coll2,data2)]):
+        i+=1
+        inputs[f'coll{i}_ids'] = data['ids']
+        inputs[f'coll{i}_rids'] = [record['record_id'] for record in data['metadatas']]
+        inputs[f'coll{i}_emb'] = np.matrix(data['embeddings'])
+        inputs[f'coll{i}_len'] = len(data['ids'])
+        inputs[f'coll{i}_name'] = name
+    
+    return inputs
+
+def compute_similarities(inputs: Dict[str, Union[str, List[str], np.ndarray]],
+                                      agg_function: Literal['mean','max'],
+                                      percentile: bool, 
+                                      threshold: float, 
+                                      binarize: bool) -> Tuple[Dict[str, Union[str, List[str], np.ndarray]], np.ndarray]:
+    print('--- Get similarities ---')
+    similarities = 1 - sp.distance.cdist(inputs['coll1_emb'],inputs['coll2_emb'], 'cosine')
+    if percentile:
+        threshold = np.percentile(similarities.reshape(-1), percentile) 
+
+    print(f'--- Using {threshold} as threshold ---')
+    print('--- Aggregate similarities by record ---')
+    df = pd.DataFrame(similarities, index=inputs['coll1_rids'], columns=inputs['coll2_rids']) # 
+
+    if agg_function == 'mean':
+        similarities = df.stack().reset_index().groupby(['level_0','level_1']).mean().unstack()
+    elif agg_function == 'max':
+        similarities = df.stack().reset_index().groupby(['level_0','level_1']).max().unstack()
+    else:
+        raise Exception('Aggregation function not supported, select either mean or max')
+    #exec(f"similarities = df.stack().reset_index().groupby(['level_0','level_1']).{agg_function}().unstack()")
+    inputs['coll1_rids'],inputs['coll2_rids'] = list(similarities.index), list(similarities.columns.droplevel())
+    similarities = similarities.values
+
+    print('--- Threshold similarities and binarize ---')
+    if binarize:
+        similarities[similarities >= threshold] = 1
+        similarities[similarities < threshold] = 0
+   
+    return inputs, similarities
+
+def get_edges(db: ChromaDB,coll1: str,coll2: str,modality1: str,modality2: str,
+              agg_function: Literal['mean','max'],
+              percentile: bool=False, threshold: float=.9, binarize: bool=True):
+    print('Get inputs...')
+    inputs = get_data(db, coll1, coll2, modality1, modality2)
+    print('Compute similarities...')
+    inputs, similarities = compute_similarities(inputs,agg_function, percentile, threshold, binarize)
+    print('Retrieve edges...')
+    mapping1 = {i:j for i,j in zip(range(len(inputs['coll1_rids'])),inputs['coll1_rids'])}
+    mapping2 = {i:j for i,j in zip(range(len(inputs['coll2_rids'])),inputs['coll2_rids'])}
+
+    edges = [(mapping1[i],mapping2[j]) for i,j in zip(*np.where(similarities > 0))]
+    return edges, similarities, inputs
+
+
+def plot_query_results(results: dict, 
+                       source: str='img_path') -> pd.DataFrame:
+    """function for plotting the results of a query"""
+    source = 'img_path'
+    result_df = pd.DataFrame(results['metadatas'][0])
+    result_df['similarity'] = 1 - np.array(results['distances'][0])
+
+
+    fig = plt.figure(figsize=(10, 20))
+    columns = 2
+    rows = 5
+    for i in range(1, columns*rows +1):
+        if source == 'img_url':
+            img = Image.open(requests.get(result_df.loc[i-1,source], stream=True).raw).convert("RGB")
+        elif source == 'img_path':
+            img = Image.open(result_df.loc[i-1,source])
+        ax = fig.add_subplot(rows, columns, i,)
+        title = f"{result_df.loc[i-1,'record_id']} {result_df.loc[i-1,'similarity']:.3f}" # 
+        ax.title.set_text(title)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        plt.imshow(img)
+    plt.show()
+    return result_df
+
+def get_query_results(results: Dict, 
+                      collection_df: pd.DataFrame, 
+                      source: str='img_path') -> pd.DataFrame:
+    result_df = pd.DataFrame(results['metadatas'][0])
+    result_df['similarity'] = 1 - np.array(results['distances'][0])
+    top_results = result_df.groupby('record_id')['similarity'].max().sort_values(ascending=False)#.index.tolist()
+    return pd.DataFrame(
+                top_results
+            ).merge(
+                collection_df[['record_id',source,'description']],
+                left_index=True,
+                right_on='record_id',
+                how='left'
+                    ).reset_index(drop=True)
+    
