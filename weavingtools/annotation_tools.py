c8e16ad970283bedecb41600d5bc2e3a970ede7a
kasparvonbeelen <kasparbeelen@mac-58294.local>
2024-09-12 15:51:56 +0100

    general update

--- weavingtools/annotation_tools.py
+++ weavingtools/annotation_tools.py
@@ -1,22 +1,46 @@
 from typing import List, Tuple
 from PIL import Image
+import io
 import matplotlib.pyplot as plt
 import requests
+
+def soft_wrap_text(text: str, max_char_length: int=40) -> str:
+    words = text.split()
+    lines = []
+    current_line = ''
+    
+    for w in words:
+        if len(current_line) + len(w) <= max_char_length:
+            current_line+= ' ' + w if current_line else w
+        else:
+            lines.append(current_line)
+            current_line = w
+    
+    if current_line:
+        lines.append(current_line)
+    return '\n'.join(lines)
 
 def open_image(img_uri: str) -> Image.Image:
     if img_uri.startswith('http'):
         try:
-            img = Image.open(requests.get(img_path,  stream=True).raw,).convert('RGB')
+            
+            img = Image.open(requests.get(img_uri,  stream=True).raw,).convert('RGB')
         except:
             try:
-                img_path = 'https://www.nms.ac.uk/api/axiell?command=getcontent&server=Detail&value=' + img_path.split('value=')[-1]
-                data = requests.get(img_path)
+                
+                img_uri = 'https://www.nms.ac.uk/api/axiell?command=getcontent&server=Detail&value=' + img_uri.split('value=')[-1]
+                data = requests.get(img_uri)
                 img = Image.open(io.BytesIO(bytes(data.content)))
             except:
-                img = Image.open('./heritageweaver/data/No_Image_Available.jpg').convert("RGB")
+                no_image_uri = 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930'
+                img = Image.open(requests.get(no_image_uri,  stream=True).raw,).convert('RGB')
+                #img = Image.open('./data/No_Image_Available.jpg').convert("RGB") # ./heritageweaver/data/No_Image_Available.jpg
         #return Image.open(requests.get(img_uri, stream=True).raw).convert("RGB")
+        
         return img
+    
     return Image.open(img_uri).convert("RGB")
+    
     
     
 
@@ -30,6 +54,7 @@ def plot_by_uri(img_uri: str) -> None:
     plt.show()
 
 def plot_by_record(record: List[str]) -> None:
+    
     fig = plt.figure(figsize=(10, 10))
     img = open_image(record[3]) 
     ax = fig.add_subplot(1, 1, 1,)
@@ -37,7 +62,7 @@ def plot_by_record(record: List[str]) -> None:
     ax.set_yticks([])
     plt.imshow(img)
     plt.show()
-    print(record[-1])
+    print(soft_wrap_text(record[-1]))
 
    
 def plotting_pairs(img_pair: Tuple[str, str]) -> None:
@@ -58,18 +83,3 @@ def plotting_pairs(img_pair: Tuple[str, str]) -> None:
         
     plt.show()
 
-def soft_wrap_text(text: str, max_char_length: int=40) -> str:
-    words = text.split()
-    lines = []
-    current_line = ''
-    
-    for w in words:
-        if len(current_line) + len(w) <= max_char_length:
-            current_line+= ' ' + w if current_line else w
-        else:
-            lines.append(current_line)
-            current_line = w
-    
-    if current_line:
-        lines.append(current_line)
-    return '\n'.join(lines)
--- weavingtools/linkage_tools.py
+++ weavingtools/linkage_tools.py
@@ -28,14 +28,48 @@ def retrieve_records(db: ChromaDB,coll: str,modality: str) -> ChromaDB:
                 include=['embeddings','metadatas'] 
                     )
 
+def filter_edges_by_keywords(edges: List[Tuple[str,str]], 
+                             db: ChromaDB, 
+                             keywords: List[str],
+                             max_candidates: int=1000,) -> List[Tuple[str,str]]:
+    """function to filter edges by keywords
+    Arguments:
+        edges: list, list of edges
+        db: ChromaDB object
+        keywords: list, list of keywords to filter the edges"""
+    
+    # get the metadata for the records
+    results = db.query(
+                query_texts=keywords,
+                #include=['record_id','description'],
+                n_results=max_candidates
+                    )
+    edge_ids = [r['record_id'] for res in results['metadatas'] for r in res]
+    # filter the edges if contains edge_ids from the results
+    filtered_edges = [edge for edge in edges if edge[0] in edge_ids or edge[1] in edge_ids]
+    return filtered_edges
 
 def get_data(db: ChromaDB,
              coll1: str, coll2: str, 
              modality1: str, 
              modality2: str) -> Dict[str, Union[str, List[str], np.ndarray]]: 
+    """function to get data from two different collection from the vector databases
+    Arguments:
+        db: ChromaDB object
+        coll1: str, name of the first collection
+        coll2: str, name of the second collection
+        modality1: str, modality of the first collection
+        modality2: str, modality of the second collection
+    Returns:
+        inputs: dict, dictionary containing the following keys with {i} being either 1 or 2:
+            coll{i}_ids: list, list of ids from the database 
+            coll{i}_rids: list, list of record ids from the database
+            coll{i}_emb: np.ndarray, matrix of embeddings
+            coll{i}_len: int, length of the ids
+            coll{i}_name: str, name of the collection"""
+    
     data1 = retrieve_records(db,coll1, modality1)
     data2 = retrieve_records(db,coll2, modality2)
-    print(len(data1),len(data2))
     
     inputs = dict()
     
@@ -51,11 +85,25 @@ def get_data(db: ChromaDB,
 
 def compute_similarities(inputs: Dict[str, Union[str, List[str], np.ndarray]],
                                       agg_function: Literal['mean','max'],
-                                      percentile: bool, 
+                                      percentile: Union[bool,float], 
                                       threshold: float, 
                                       binarize: bool) -> Tuple[Dict[str, Union[str, List[str], np.ndarray]], np.ndarray]:
+    """function to compute similarities between records from two collections
+    takes as input the dictioanry produced by the get_data function
+    Arguments:
+        agg_function: str, aggregation function to use to aggregate the similarities by record
+        percentile: bool, if False, used the value for threshold argument
+            if a float, compute this threshold as the percentile cutoff of the similarities
+        threshold: float, value to use as threshold if percentile is False
+        binarize: bool, if True, binarize the similarities into edges
+    """
+
     print('--- Get similarities ---')
+    # get coisine similarity between embeddings matrices
     similarities = 1 - sp.distance.cdist(inputs['coll1_emb'],inputs['coll2_emb'], 'cosine')
+
+    # we need to select a cutoff threshold to binarize the similarities into edges 
+    # if percentile is True, compute the threshold as the percentile of the similarities
     if percentile:
         threshold = np.percentile(similarities.reshape(-1), percentile) 
 
@@ -63,18 +111,20 @@ def compute_similarities(inputs: Dict[str, Union[str, List[str], np.ndarray]],
     print('--- Aggregate similarities by record ---')
     df = pd.DataFrame(similarities, index=inputs['coll1_rids'], columns=inputs['coll2_rids']) # 
 
+    # to compute the similarity by object we need to aggragate the similarities by record
+    # we can either take the mean or the max of the similarities
     if agg_function == 'mean':
         similarities = df.stack().reset_index().groupby(['level_0','level_1']).mean().unstack()
     elif agg_function == 'max':
         similarities = df.stack().reset_index().groupby(['level_0','level_1']).max().unstack()
     else:
         raise Exception('Aggregation function not supported, select either mean or max')
-    #exec(f"similarities = df.stack().reset_index().groupby(['level_0','level_1']).{agg_function}().unstack()")
+    
     inputs['coll1_rids'],inputs['coll2_rids'] = list(similarities.index), list(similarities.columns.droplevel())
     similarities = similarities.values
 
-    print('--- Threshold similarities and binarize ---')
     if binarize:
+        print('--- Threshold similarities and binarize ---')
         similarities[similarities >= threshold] = 1
         similarities[similarities < threshold] = 0
    
@@ -82,22 +132,43 @@ def compute_similarities(inputs: Dict[str, Union[str, List[str], np.ndarray]],
 
 def get_edges(db: ChromaDB,coll1: str,coll2: str,modality1: str,modality2: str,
               agg_function: Literal['mean','max'],
-              percentile: bool=False, threshold: float=.9, binarize: bool=True):
+              percentile: Union[bool,float]=False, 
+              threshold: float=.9, 
+              binarize: bool=True):
+    """function to get edges from two collections builds 
+    on the compute_similarities and get_datafunction
+    Arguments:
+        db: ChromaDB object
+        coll1: str, name of the first collection
+        coll2: str, name of the second collection
+        modality1: str, modality of the first collection
+        modality2: str, modality of the second collection
+        agg_function: str, aggregation function to use to aggregate the similarities by record
+        percentile: bool, if False, used the value for threshold argument
+            if a float, compute this threshold as the percentile cutoff of the similarities
+        threshold: float, value to use as threshold if percentile is False
+        binarize: bool, if True, binarize the similarities into edges"""
+    
     print('Get inputs...')
     inputs = get_data(db, coll1, coll2, modality1, modality2)
     print('Compute similarities...')
     inputs, similarities = compute_similarities(inputs,agg_function, percentile, threshold, binarize)
     print('Retrieve edges...')
+    # we need to map the indices to the record ids
     mapping1 = {i:j for i,j in zip(range(len(inputs['coll1_rids'])),inputs['coll1_rids'])}
     mapping2 = {i:j for i,j in zip(range(len(inputs['coll2_rids'])),inputs['coll2_rids'])}
-
+    # get the edges from the similarities matrix 
+    # assumuses a binarized matrix with edges as non-zero values
     edges = [(mapping1[i],mapping2[j]) for i,j in zip(*np.where(similarities > 0))]
     return edges, similarities, inputs
 
 
-def plot_query_results(results: dict, 
-                       source: str='img_path') -> pd.DataFrame:
-    """function for plotting the results of a query"""
+def plot_query_results(results: Dict, 
+                       source :Literal['img_path', 'img_url'] = 'img_path') -> pd.DataFrame:
+    """function for plotting the results of a query
+    Arguments:
+        results: dict, dictionary containing the results of the query
+        source: str, source of the images, either 'img_path' or 'img_url'"""
     result_df = pd.DataFrame(results['metadatas'][0])
     result_df['similarity'] = 1 - np.array(results['distances'][0])
 
@@ -115,9 +186,12 @@ def plot_query_results(results: dict,
                     data = requests.get(img_path)
                     img = Image.open(io.BytesIO(bytes(data.content)))
                 except:
-                    img = Image.open('./heritageweaver/data/No_Image_Available.jpg').convert("RGB")
+                    #img = Image.open('./heritageweaver/data/No_Image_Available.jpg').convert("RGB")
+                    no_image_uri = 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930'
+                    img = Image.open(requests.get(no_image_uri,  stream=True).raw,).convert('RGB')
         elif source == 'img_path':
             img = Image.open(result_df.loc[i-1,source]).convert("RGB")
+        
         ax = fig.add_subplot(rows, columns, i,)
         title = f"{result_df.loc[i-1,'record_id']} {result_df.loc[i-1,'similarity']:.3f}" # 
         ax.title.set_text(title)
@@ -129,8 +203,13 @@ def plot_query_results(results: dict,
 
 def get_query_results(results: Dict, 
                       collection_df: pd.DataFrame, 
-                      source: str='img_path'
+                      source: Literal['img_path', 'img_url'] = 'img_path'
                       ) -> pd.DataFrame:
+    """function to get the results of a query
+    Arguments:
+        results: dict, dictionary containing the results of the query
+        collection_df: pd.DataFrame, dataframe containing the collection metadata
+        source: str, source of the images, either 'img_path' or 'img_url'"""
     result_df = pd.DataFrame(results['metadatas'][0])
     result_df['similarity'] = 1 - np.array(results['distances'][0])
     #result_df = result_df[['record_id',source,'modality','name','similarity']]
