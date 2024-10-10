a3627408dce6a90e088977e4314f49cfce072d7f
kasparvonbeelen <kasparbeelen@mac-58294.local>
2024-10-10 10:49:14 +0100

    update db info

--- create_env.sh
+++ create_env.sh
@@ -3,6 +3,6 @@ pip install -qr requirements.txt
 # download data database and some additional metadata
 cd /content/
 pip install --upgrade --no-cache-dir gdown
-gdown 1r8ddjSuhtC4ZEmcEu15o5TV2dX35Qj12
+gdown 1_JnLjNDZV-yv5BbRIxK2MeVxyvE-5o0U
 # unzip archive with vector database
-unzip -qq -o hw.zip
+unzip -qq -o hw-08-10.zip
